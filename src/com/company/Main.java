e091e4e3b8ad65e2c5052a6c005377b1b5bf2152
PlotnikovaNastya <36679061+PlotnikovaNastya@users.noreply.github.com>
2020-10-29 19:23:37 +0300

    6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,8 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Первая задача:");
-        block4_z1 b4_z1 = new block4_z1();
-        b4_z1.task1();
+        block6_z1 b6_z1 = new block6_z1();
+        b6_z1.task1();
         /*System.out.println("Вторая задача:");
         /*block1_z2 b1_z2=new block1_z2();
         b1_z2.task2();
