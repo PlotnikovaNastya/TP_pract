85367bdb0ab0833b9130a169139a5c98cb4ae1fb
PlotnikovaNastya <36679061+PlotnikovaNastya@users.noreply.github.com>
2020-11-06 17:07:21 +0300

    Задача 2 блока 4

--- src/com/company/block4_z1.java
+++ src/com/company/block4_z1.java
@@ -3,16 +3,34 @@ import java.util.Scanner;
 public class block4_z1 {
     public static void task1() {
         Scanner in = new Scanner(System.in);
-        System.out.println("Введите первый параметр:");
-        int a = in.nextInt();
-        System.out.println("Введите второй параметр:");
-        int b = in.nextInt();
-        int rez_div = division(a, b);
-        System.out.println("Остаток от деления равен " + rez_div);
+        System.out.println("Введите количество слов (от 1 до 100):");
+        int n = in.nextInt();
+        System.out.println("Введите количество символов в строке (от 1 до 80):");
+        int k = in.nextInt();
+        System.out.println("Введите строку:");
+        String str = in.nextLine();
+        System.out.println("Результат:\n" +bes(n,k,str));
     }
 
-    public static int division(int a, int b)
+    public static String bes(int n, int k, String str)
     {
-        return a % b;
+        String[] s=str.split(" ");
+        str="";
+        String new_s = "";
+        //if ((n>=1 & n<=100) && (k >= 1 & k <= 80))
+        //{
+        for(int i = 0; i < n; i++) {
+                if ((new_s.length() + s[i].length()) < k)
+                {
+                    str += s[i] + " ";
+                    new_s += s[i];
+                }
+                else {
+                    str = str.trim() + "\r\n" + s[i] + " ";
+                    new_s = s[i];
+                }
+            }
+        // } else System.out.println("Недопустимое количество слов или символов");
+        return str.trim();
     }
 }
