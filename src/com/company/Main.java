2d5d6289d81af8525320b31dec16bf8bd147aea9
PlotnikovaNastya <36679061+PlotnikovaNastya@users.noreply.github.com>
2020-11-09 23:27:07 +0300

    Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,31 +5,31 @@ public class Main {
         block4_z1 b4_z1 = new block4_z1();
         b4_z1.task1();
         /*System.out.println("Вторая задача:");
-        /*block1_z2 b1_z2=new block1_z2();
-        b1_z2.task2();
+        block4_z2 b4_z2=new block4_z2();
+        b4_z2.task2();
         System.out.println("Третья задача:");
-        block1_z3 b1_z3=new block1_z3();
-        b1_z3.task3();
+        block4_z3 b4_z3=new block4_z3();
+        b4_z3.task3();
         System.out.println("Четвертая задача:");
-        block1_z4 b1_z4=new block1_z4();
-        b1_z4.task4();
+        block4_z4 b4_z4=new block4_z4();
+        b4_z4.task4();
         System.out.println("Пятая задача:");
-        block1_z5 b1_z5=new block1_z5();
-        b1_z5.task5();
+        block4_z5 b4_z5=new block4_z5();
+        b4_z5.task5();
         System.out.println("Шестая задача:");
-        block1_z6 b1_z6=new block1_z6();
-        b1_z6.task6();
+        block4_z6 b4_z6=new block4_z6();
+        b4_z6.task6();
         System.out.println("Седьмая задача:");
-        block1_z7 b1_z7=new block1_z7();
-        b1_z7.task7();
+        block4_z7 b4_z7=new block4_z7();
+        b4_z7.task7();
         System.out.println("Восьмая задача:");
-        block1_z8 b1_z8=new block1_z8();
-        b1_z8.task8();
+        block4_z8 b4_z8=new block4_z8();
+        b4_z8.task8();
         System.out.println("Девятая задача:");
-        block1_z9 b1_z9=new block1_z9();
-        b1_z9.task9();
+        block4_z9 b4_z9=new block4_z9();
+        b4_z9.task9();
         System.out.println("Десятая задача:");
-        block1_z10 b1_z10=new block1_z10();
-        b1_z10.task10();*/
+        block4_z10 b4_z10=new block4_z10();
+        b4_z10.task10();*/
     }
 }
