ee152753381ede57ebf33fb0a43b0263fbc98cce
PlotnikovaNastya <36679061+PlotnikovaNastya@users.noreply.github.com>
2020-11-09 23:25:55 +0300

    Задача 1 блока 4

--- src/com/company/block4_z1.java
+++ src/com/company/block4_z1.java
@@ -4,33 +4,33 @@ public class block4_z1 {
     public static void task1() {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите количество слов (от 1 до 100):");
-        int n = in.nextInt();
+        int n = Integer.parseInt(in.nextLine());
         System.out.println("Введите количество символов в строке (от 1 до 80):");
-        int k = in.nextInt();
+        int k = Integer.parseInt(in.nextLine());
         System.out.println("Введите строку:");
-        String str = in.nextLine();
-        System.out.println("Результат:\n" +bes(n,k,str));
+        String st = in.nextLine();
+        System.out.println(bessi(n,k,st));
     }
 
-    public static String bes(int n, int k, String str)
+    public static String bessi(int n, int k, String str)
     {
-        String[] s=str.split(" ");
-        str="";
+        String[] s=str.split(" ");//разделение строки по пробелам
         String new_s = "";
-        //if ((n>=1 & n<=100) && (k >= 1 & k <= 80))
-        //{
+        str="";
+        if ((n>=1 & n<=100) && (k >= 1 & k <= 80))
+        {
         for(int i = 0; i < n; i++) {
-                if ((new_s.length() + s[i].length()) < k)
+                if (new_s.length() + s[i].length() > k)//trim - убирает пробелы
                 {
-                    str += s[i] + " ";
-                    new_s += s[i];
-                }
-                else {
                     str = str.trim() + "\r\n" + s[i] + " ";
                     new_s = s[i];
                 }
+                else {
+                    str += s[i] + " ";
+                    new_s += s[i];
+                }
             }
-        // } else System.out.println("Недопустимое количество слов или символов");
+        } else System.out.println("Недопустимое количество слов или символов");
         return str.trim();
     }
 }
