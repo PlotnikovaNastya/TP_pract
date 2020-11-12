82ca34d698d5f3c42032623116d080f3dffdd3de
PlotnikovaNastya <36679061+PlotnikovaNastya@users.noreply.github.com>
2020-11-12 21:39:32 +0300

    Задача 6 блока 5

---
+++ src/com/company/block5_z6.java
@@ -0,0 +1,39 @@
+package com.company;
+import java.util.Scanner;
+public class block5_z6 {
+    public static void task6() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число:");
+        String n = in.nextLine();
+        in.close();
+        System.out.println("Это действительно номер кредитной карты? " + card(n));
+    }
+
+    public static boolean card(String n)
+    {
+        long nn=Long.valueOf(n);
+        if (n.length()>=14 && n.length()<=19) {
+            System.out.println("Условие выполнилось");
+            int kc = (int) (nn % 10);
+            nn = nn / 10;
+            int sum = 0;
+            String new_n = Long.toString(nn);
+            StringBuffer buffer = new StringBuffer(new_n);//необходимо для изменения строки без создания новой
+            buffer.reverse();//изменение порядка строки
+            for (int i = 0; i < new_n.length(); i++) {
+                if (i % 2 == 0) {
+                    int pos = Character.getNumericValue(new_n.charAt(i)) * 2;//символ в число
+                    sum += pos / 10;//первая цифра числа
+                    sum += pos % 10;//вторая цифра числа
+                } else
+                    sum += Character.getNumericValue(new_n.charAt(i));
+            }
+            int en=10-sum % 10;
+            if (en == kc)
+                return true;
+            else return false;
+        }
+        else
+            return false;
+    }
+}
