7c8fddaf87b43e4f3cfa8a6e383585724c00b626
PlotnikovaNastya <36679061+PlotnikovaNastya@users.noreply.github.com>
2020-11-19 17:18:18 +0300

    Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,35 +1,6 @@
 package com.company;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Первая задача:");
-        block6_z1 b6_z1 = new block6_z1();
-        b6_z1.task1();
-        /*System.out.println("Вторая задача:");
-        /*block1_z2 b1_z2=new block1_z2();
-        b1_z2.task2();
-        System.out.println("Третья задача:");
-        block1_z3 b1_z3=new block1_z3();
-        b1_z3.task3();
-        System.out.println("Четвертая задача:");
-        block1_z4 b1_z4=new block1_z4();
-        b1_z4.task4();
-        System.out.println("Пятая задача:");
-        block1_z5 b1_z5=new block1_z5();
-        b1_z5.task5();
-        System.out.println("Шестая задача:");
-        block1_z6 b1_z6=new block1_z6();
-        b1_z6.task6();
-        System.out.println("Седьмая задача:");
-        block1_z7 b1_z7=new block1_z7();
-        b1_z7.task7();
-        System.out.println("Восьмая задача:");
-        block1_z8 b1_z8=new block1_z8();
-        b1_z8.task8();
-        System.out.println("Девятая задача:");
-        block1_z9 b1_z9=new block1_z9();
-        b1_z9.task9();
-        System.out.println("Десятая задача:");
-        block1_z10 b1_z10=new block1_z10();
-        b1_z10.task10();*/
+
     }
 }
