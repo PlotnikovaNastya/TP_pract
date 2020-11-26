02bc6f755c72b7b95caf40706b08c7ec608b8729
PlotnikovaNastya <36679061+PlotnikovaNastya@users.noreply.github.com>
2020-11-26 19:06:57 +0300

    Задача 8 блока 6

--- src/com/company/block6_z8.java
+++ src/com/company/block6_z8.java
@@ -1,5 +1,66 @@
 package com.company;
-
 public class block6_z8 {
-    public static void task8() {}
+    public static void task8() {
+        System.out.println(rim_num(16));
+    }
+    public static String rim_num(int n) {
+        String res = "";
+        if (n>0 && n<=3999) {
+            while (n >= 1000) {
+                res += "M";
+                n -= 1000;
+            }
+            while (n >= 900) {
+                res += "CM";
+                n -= 900;
+            }
+            while (n >= 500) {
+                res += "D";
+                n -= 500;
+            }
+            while (n >= 400) {
+                res += "CD";
+                n -= 400;
+            }
+            while (n >= 100) {
+                res += "C";
+                n -= 100;
+            }
+            while (n >= 90) {
+                res += "XC";
+                n -= 90;
+            }
+            while (n >= 50) {
+                res += "L";
+                n -= 50;
+            }
+            while (n >= 40) {
+                res += "XL";
+                n -= 40;
+            }
+            while (n >= 10) {
+                res += "X";
+                n -= 10;
+            }
+            while (n == 9) {
+                res += "IX";
+                n -= 9;
+            }
+            while (n >= 5) {
+                res += "V";
+                n -= 5;
+            }
+            while (n == 4) {
+                res += "IV";
+                n -= 4;
+            }
+            while (n >= 1) {
+                res += "I";
+                n -= 1;
+            }
+
+        }
+        else System.out.println("Несоответствующее число!");
+        return res;
+    }
 }
