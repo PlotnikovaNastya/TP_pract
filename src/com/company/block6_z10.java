2c270c633306cf1f39c4a70c88b1b9ad05d46ac6
PlotnikovaNastya <36679061+PlotnikovaNastya@users.noreply.github.com>
2020-11-26 22:55:20 +0300

    Задача 10 блока 6

--- src/com/company/block6_z10.java
+++ src/com/company/block6_z10.java
@@ -1,5 +1,47 @@
 package com.company;
 
 public class block6_z10 {
-    public static void task10() {}
+    public static void task10() {
+        System.out.println(polin(23336014));
+    }
+
+    public static boolean polin(int n)
+    {
+        String s = Integer.toString(n);
+        while (true)
+        {
+            if (s.length() < 2)
+            {
+                return false;
+            }
+
+            if (s.length() % 2 != 0)
+            {
+                if (s.substring(0, s.length()/2).equals(new StringBuilder(s.substring(s.length()/2+1)).reverse().toString()))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            if (s.substring(0, s.length()/2).equals(new StringBuilder(s.substring(s.length()/2)).reverse().toString()))
+            {
+                return true;
+            }
+            else
+            {
+                String buff = "";
+                for (int i = 0; i < s.length(); i += 2)
+                {
+                    buff += Integer.toString(Integer.parseInt(Character.toString(s.charAt(i))) +
+                            Integer.parseInt(Character.toString(s.charAt(i+1))));
+                }
+                s = buff;
+            }
+        }
+    }
 }
+
