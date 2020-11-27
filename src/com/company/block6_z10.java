65edafad950614b44909a33af0523e49a9846283
PlotnikovaNastya <36679061+PlotnikovaNastya@users.noreply.github.com>
2020-11-27 12:56:03 +0300

    Задача 10 блока 6

--- src/com/company/block6_z10.java
+++ src/com/company/block6_z10.java
@@ -8,38 +8,24 @@ public class block6_z10 {
     public static boolean polin(int n)
     {
         String s = Integer.toString(n);
-        while (true)
-        {
-            if (s.length() < 2)
-            {
+        while (true) {
+            if (s.length() < 2) {
                 return false;
             }
-
-            if (s.length() % 2 != 0)
-            {
-                if (s.substring(0, s.length()/2).equals(new StringBuilder(s.substring(s.length()/2+1)).reverse().toString()))
-                {
-                    return true;
-                }
-                else
-                {
+            if (s.length() % 2 != 0 && s.length() != 2 ) {
                     return false;
-                }
             }
-
-            if (s.substring(0, s.length()/2).equals(new StringBuilder(s.substring(s.length()/2)).reverse().toString()))
-            {
-                return true;
-            }
-            else
-            {
-                String buff = "";
-                for (int i = 0; i < s.length(); i += 2)
-                {
-                    buff += Integer.toString(Integer.parseInt(Character.toString(s.charAt(i))) +
-                            Integer.parseInt(Character.toString(s.charAt(i+1))));
+            if (s.length() % 2 == 0) {
+                if (s.substring(0, s.length() / 2).equals(new StringBuilder(s.substring(s.length() / 2)).reverse().toString())) {
+                    return true;
+                } else {
+                    String buff = "";
+                    for (int i = 0; i < s.length(); i += 2) {
+                        buff += Integer.toString(Integer.parseInt(Character.toString(s.charAt(i))) +
+                                Integer.parseInt(Character.toString(s.charAt(i + 1))));
+                    }
+                    s = buff;
                 }
-                s = buff;
             }
         }
     }
