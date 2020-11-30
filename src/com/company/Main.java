03682e935ce4b24ee50bacda7ace2f4c033d2780
PlotnikovaNastya <36679061+PlotnikovaNastya@users.noreply.github.com>
2020-11-30 23:40:35 +0300

    Лабораторная 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
-
+import java.io.IOException;
+import java.util.*;
 public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        /*System.out.println("Введите глубину");
+        Scanner input = new Scanner(System.in);
+        int g =input.nextInt();*/
+        Crawler craw = new Crawler();
+        //craw.scanWebSite("http://forum-1c.ru/",3);
+        //craw.scanWebSite("http://firelied.ucoz.ru/",3);
+        craw.scanWebSite("http://old.code.mu/sql/",3);
 
-    public static void main(String[] args) {
-	// write your code here
     }
 }
